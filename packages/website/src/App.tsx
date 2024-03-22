260a118843f5dd0137960820b9b2b7b7051bcc7c
Nick Rodgers <nick.rodgers42@gmail.com>
2024-03-22 19:21:18 -0400

    Re-add web buttons

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { GetAuthenticatedCommand, PingCommand, RunningClient } from "@running/client";
+
+const client = new RunningClient({
+  endpoint: "http://localhost:8080",
+  region: "fake-region",
+});
 
 function App() {
+  async function ping() {
+    const response = await client.send(new PingCommand({}));
+    console.log(response);
+  }
+
+  async function authenticate() {
+    const response = await client.send(
+      new GetAuthenticatedCommand({
+        username: "localuser",
+      }),
+    );
+    console.log(response)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <button
+        onClick={() => {
+          ping();
+        }}
+      >
+        Click to ping
+      </button>
+      <button onClick={() => { authenticate() }}>Click to authenticate</button>
     </div>
   );
 }
