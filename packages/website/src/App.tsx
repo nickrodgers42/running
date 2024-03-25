aefb2702e0a0e06ae839bd8955de2d7b46cdb7ae
Nick Rodgers <nick.rodgers42@gmail.com>
2024-03-24 23:57:08 -0400

    Use nextui

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,29 +1,30 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react"
+import "./App.css"
 import {
     GetAuthenticatedCommand,
     PingCommand,
     RunningClient,
-} from "@running/client";
+} from "@running/client"
+import { NextUIProvider } from "@nextui-org/react"
 
 const client = new RunningClient({
     endpoint: "http://localhost:8080",
     region: "fake-region",
-});
+})
 
 function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-    const [authenticateUrl, setAuthenticateUrl] = useState<string>("");
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
+    const [authenticateUrl, setAuthenticateUrl] = useState<string>("")
 
     useEffect(() => {
         if (!isAuthenticated) {
-            checkAuthenticated();
+            checkAuthenticated()
         }
-    }, [isAuthenticated]);
+    }, [isAuthenticated])
 
     async function ping() {
-        const response = await client.send(new PingCommand({}));
-        console.log(response);
+        const response = await client.send(new PingCommand({}))
+        console.log(response)
     }
 
     async function checkAuthenticated() {
@@ -31,33 +32,35 @@ function App() {
             new GetAuthenticatedCommand({
                 username: "localuser",
             }),
-        );
+        )
         if (response.isAuthenticated === undefined) {
-            return;
+            return
         }
-        setIsAuthenticated(response.isAuthenticated);
+        setIsAuthenticated(response.isAuthenticated)
         if (
             response.isAuthenticated === false &&
             response.authUrl !== undefined
         ) {
-            setAuthenticateUrl(response.authUrl);
+            setAuthenticateUrl(response.authUrl)
         }
     }
 
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-            <p>You are {isAuthenticated ? "" : "not "}authenticated</p>
-            <button onClick={ping}>Click to ping</button>
-            <button
-                onClick={() => {
-                    window.location.href = authenticateUrl;
-                }}
-            >
-                Click to authenticate
-            </button>
-        </div>
-    );
+        <NextUIProvider>
+            <div className="App">
+                <h1>Hello World</h1>
+                <p>You are {isAuthenticated ? "" : "not "}authenticated</p>
+                <button onClick={ping}>Click to ping</button>
+                <button
+                    onClick={() => {
+                        window.location.href = authenticateUrl
+                    }}
+                >
+                    Click to authenticate
+                </button>
+            </div>
+        </NextUIProvider>
+    )
 }
 
-export default App;
+export default App
